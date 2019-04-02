0f3f6c38cf1fef9dc9e9d1ac46f3d885257e7faa
Nikola Stoyanov <nstoyanov.92@gmail.com>
2019-04-02 19:22:57 +0100

    Fix git push path

--- _scripts/update.sh
+++ _scripts/update.sh
@@ -19,7 +19,7 @@ git add .
 if ! git diff --quiet --staged
   then
     git commit -m "Updated from Strava, `date +%Y/%m/%d`."
-    git push "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git HEAD:master"
+    git push "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" HEAD:master
  else
    echo "Nothing has changed! I hope that's what you expected." >&2
 fi
