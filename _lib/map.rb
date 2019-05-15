6983ddca96c7d02e793475113bb08b24eae09479
Nikola Stoyanov <nstoyanov.92@gmail.com>
2019-05-15 13:56:47 +0100

    Change map scale for higher resolution

--- _lib/map.rb
+++ _lib/map.rb
@@ -11,6 +11,6 @@ class Strava::Models::Map < Strava::Model
   
       start_latlng = decoded_summary_polyline[0]
       end_latlng = decoded_summary_polyline[-1]
-      "https://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&path=enc:#{summary_polyline}&key=#{google_maps_api_key}&size=800x800&markers=color:yellow|label:S|#{start_latlng[0]},#{start_latlng[1]}&markers=color:green|label:F|#{end_latlng[0]},#{end_latlng[1]}"
+      "https://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&path=enc:#{summary_polyline}&key=#{google_maps_api_key}&size=800x800&scale=2&markers=color:yellow|label:S|#{start_latlng[0]},#{start_latlng[1]}&markers=color:green|label:F|#{end_latlng[0]},#{end_latlng[1]}"
     end
   end
